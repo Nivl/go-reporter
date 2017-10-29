5a705c3c1e8c3c4aee5958b1b110c05c9ad107ba
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-10-29 00:23:52 -0700

    chore: import code from grt

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # go-reporter
+
+[![Build Status](https://travis-ci.org/Nivl/go-reporter.svg)](https://travis-ci.org/Nivl/go-reporter)
+[![Go Report Card](https://goreportcard.com/badge/github.com/nivl/go-reporter)](https://goreportcard.com/report/github.com/nivl/go-reporter)
+[![GoDoc](https://godoc.org/github.com/Nivl/go-reporter?status.svg)](https://godoc.org/github.com/Nivl/go-reporter)
+
+## implementations
+
+* [Sentry](https://godoc.org/github.com/Nivl/go-reporter/implementations/sentryreporter)
+* [Mailer](https://godoc.org/github.com/Nivl/go-reporter/implementations/mailerreporter)
+* [No-Op](https://godoc.org/github.com/Nivl/go-reporter/implementations/noopreporter)
+* [GoMock](https://godoc.org/github.com/Nivl/go-reporter/implementations/mockreporter)
---
+++ implementations/sentryreporter/reporter_integration_test.go
@@ -0,0 +1,63 @@
+package sentryreporter_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/dchest/uniuri"
+
+	uuid "github.com/satori/go.uuid"
+
+	"github.com/stretchr/testify/require"
+
+	reporter "github.com/Nivl/go-reporter"
+	"github.com/Nivl/go-reporter/implementations/sentryreporter"
+)
+
+func TestSentryHappyPath(t *testing.T) {
+	sentryDSN := os.Getenv("SENTRY_DSN")
+	if sentryDSN == "" {
+		t.Skip("sentry not set")
+	}
+
+	creator, err := sentryreporter.NewCreator(sentryDSN)
+	require.NoError(t, err, "NewCreator() should not have failed")
+
+	r, err := creator.New()
+	require.NoError(t, err, "creator.New() should not have failed")
+
+	// Set some data
+	r.SetUser(&reporter.User{
+		ID:       uniuri.New(),
+		Username: uniuri.New(),
+		Email:    uniuri.New(),
+		IP:       uniuri.New(),
+	})
+	r.AddTag("endpoint", "TestSentryHappyPath")
+	r.AddTag("Req ID", uuid.NewV4().String())
+
+	// send the request
+	r.ReportErrorAndWait(fmt.Errorf("New test error %s", uniuri.NewLen(4)))
+}
+
+func TestSentryNilUser(t *testing.T) {
+	sentryDSN := os.Getenv("SENTRY_DSN")
+	if sentryDSN == "" {
+		t.Skip("sentry not set")
+	}
+
+	creator, err := sentryreporter.NewCreator(sentryDSN)
+	require.NoError(t, err, "NewCreator() should not have failed")
+
+	r, err := creator.New()
+	require.NoError(t, err, "creator.New() should not have failed")
+
+	// Set some data
+	r.SetUser(nil)
+	r.AddTag("endpoint", "TestSentryHappyPath")
+	r.AddTag("Req ID", uuid.NewV4().String())
+
+	// send the request
+	r.ReportErrorAndWait(fmt.Errorf("New test error %s", uniuri.NewLen(4)))
+}
